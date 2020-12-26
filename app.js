9e2c005433e3e3efd7cf01ba7b29fcc13257f893
kimjinsoo <roorooro9933@gmail.com>
2020-12-26 14:57:24 +0900

    add backdrop and click event, and using anchor tag for navigation

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const sideNav = document.querySelector(".side-nav");
 const xIcon = document.querySelector(".x-icon");
 const hamberger = document.querySelector(".hamberger");
+const backdrop = document.querySelector(".backdrop");
 
 // setting time line!
 const tl = gsap.timeline({ default: { ease: "power1.out" } });
 
-hamberger.addEventListener("click", () => {
+function openSideNav() {
   tl.to(".side-nav", { x: "0%", duration: 0.5 });
   tl.to(".nav__link a", { y: "0%", duration: 1 });
-});
+  backdrop.style.display = "block";
+}
 
-xIcon.addEventListener("click", () => {
+function closeSideNav() {
   tl.to(".side-nav", { x: "-100%", duration: 0.5 });
   tl.to(".nav__link a", { y: "100%", duration: 1 });
-});
+  backdrop.style.display = "none";
+}
+
+hamberger.addEventListener("click", openSideNav);
+xIcon.addEventListener("click", closeSideNav);
+backdrop.addEventListener("click", closeSideNav);
